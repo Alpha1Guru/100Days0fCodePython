4bf083d5cecdbdff8e200057fe34868e347344b2
Alpha1Guru <109897479+Alpha1Guru@users.noreply.github.com>
2025-08-17 21:41:39 +0100

    Changed something in the random_love_calculator

--- Day_4/Random_love_calculator2.py
+++ Day_4/Random_love_calculator2.py
@@ -7,7 +7,7 @@ name2 = input("What is their name? ")
 combined_name = name1.lower() + name2.lower()
 
 
-love_score = random.randint(0,101)
+love_score = random.randint(0,100)
 
 if love_score < 10 or love_score > 90:
     print(f"Your score is {love_score}, you go together like coke and mentos")
